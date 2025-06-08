749248b4966892b2d393b566b1b3ab697182f00e
lmellan <lorna.mella@sansano.usm.cl>
2025-06-08 16:41:56 -0400

    Test: Actualización Jenkins a rama develop p1

--- frontend-conectados/src/App.test.js
+++ frontend-conectados/src/App.test.js
@@ -2,8 +2,9 @@ import { render, screen } from '@testing-library/react';
 import { act } from 'react'; // Importar el act moderno
 import App from './App';
 
+
 test('renders learn react link', () => {
-  act(() => {
+  act(() => { 
     render(<App />);
   });
 });
